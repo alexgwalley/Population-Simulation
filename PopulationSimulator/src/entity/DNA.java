c13cf7d5be74554d3b28856b57805d8de9cd2a5a
Phig Newton <51300860+AshtonKeith@users.noreply.github.com>
2020-02-09 03:47:14 -0500

    Added Mutation and Combination

--- PopulationSimulator/src/entity/DNA.java
+++ PopulationSimulator/src/entity/DNA.java
@@ -3,6 +3,8 @@ package entity;
 import java.awt.Color;
 import java.util.HashMap;
 
+import generator.PercentGenerator;
+
 public class DNA {
 	
 	public static final DefaultPreyDNA defaultPrey = new DefaultPreyDNA();
@@ -84,8 +86,43 @@ public class DNA {
 	}
 	
 	public static DNA combine(Animal a1, Animal a2) {
-		//TODO:  Write this.
-		return a1.getDna();
+		DNA d1 = a1.getDna();
+		DNA d2 = a2.getDna();
+		
+		String species = d1.getSpecies();
+		float weight = PercentGenerator.newPercent();
+		float mutationRate = weight*d1.getMutationRate()+(1-weight)*d2.getMutationRate();
+		weight = PercentGenerator.newPercent();
+		float scale = PercentGenerator.relPercent(mutationRate);
+		float colorRed = (weight*d1.getColor().getRed()+(1-weight)*d2.getColor().getRed())*scale;
+		weight = PercentGenerator.newPercent();
+		scale = PercentGenerator.relPercent(mutationRate);
+		float colorBlue = (weight*d1.getColor().getBlue()+(1-weight)*d2.getColor().getBlue())*scale;
+		weight = PercentGenerator.newPercent();
+		scale = PercentGenerator.relPercent(mutationRate);
+		float colorGreen = (weight*d1.getColor().getGreen()+(1-weight)*d2.getColor().getGreen())*scale;
+		HashMap<String, Integer> food = d1.getFood();
+		weight = PercentGenerator.newPercent();
+		scale = PercentGenerator.relPercent(mutationRate);
+		float fieldOfViewAngle = (weight*d1.getFieldOfViewAngle()+(1-weight)*d2.getFieldOfViewAngle())*scale;
+		weight = PercentGenerator.newPercent();
+		scale = PercentGenerator.relPercent(mutationRate);
+		int fieldOfViewRadius = (int) ((weight*d1.getFieldOfViewRadius()+(1-weight)*d2.getFieldOfViewRadius())*scale);
+		weight = PercentGenerator.newPercent();
+		scale = PercentGenerator.relPercent(mutationRate);
+		int moveSpeed = (int) ((weight*d1.getMoveSpeed()+(1-weight)*d2.getMoveSpeed())*scale);
+		weight = PercentGenerator.newPercent();
+		int radius = (int) ((weight*d1.getRadius()+(1-weight)*d2.getRadius())*scale);
+		weight = PercentGenerator.newPercent();
+		scale = PercentGenerator.relPercent(mutationRate);
+		int eatingRate = (int) ((weight*d1.getEatingRate()+(1-weight)*d2.getEatingRate()(*scale));
+		weight = PercentGenerator.newPercent();
+		scale = PercentGenerator.relPercent(mutationRate);
+		int fleeRadius = (int) ((weight*d1.getFleeRadius()+(1-weight)*d2.getFleeRadius())*scale);
+		weight = PercentGenerator.newPercent();
+		scale = PercentGenerator.relPercent(mutationRate);
+		int matingMinimum = (int) ((weight*d1.getMatingMinimum()+(1-weight)*d2.getMatingMinimum())*scale);
+		return new DNA(species, new Color((int)colorRed, (int)colorGreen, (int)colorBlue),food,fieldOfViewAngle,fieldOfViewRadius,moveSpeed,radius,mutationRate,eatingRate,fleeRadius,matingMinimum);
 	}
 
 }
